1bd65f519db8df14d4bb3cdcdaf7c22f1c39138a
pranjal <pranjal@pranjals-MacBook-Air.local>
2025-07-25 23:30:21 +0530

    dvc added

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_03_training import ModelTrainingPipeline
+from cnnClassifier.pipeline.stage_04_evaluation import EvaluationPipeline
+
 
 # --- Patch for old models with 'fn' in CategoricalCrossentropy ---
 import tensorflow as tf
@@ -68,3 +70,17 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+
+STAGE_NAME = "Evaluation stage"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evaluation = EvaluationPipeline()
+   model_evaluation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+except Exception as e:
+        logger.exception(e)
+        raise e
